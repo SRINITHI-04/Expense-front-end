a8010e24f423ad20bc44c11703c09e581585e030
SRINITHI-04 <srinithikarunanithi2004@gmail.com>
2024-02-14 15:23:17 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
-import {useEffect, useState } from 'react'
-import ExpenseItem from "./Components/ExpenseItem"
-import ExpenseForm from "./Components/ExpenseForm";
+import { useEffect, useState } from "react"
+import ExpenseItem from "./components/ExpenseItem"
+import ExpenseForm from "./components/ExpenseForm"
+import axios from "axios"
 
 const App = ()=> {
-  const [expenses,setExpenses] = useState ([{
-    id: 1,title : "Food",amount:-960},
-   {
-    id: 2,title:"Movie",amount:-200},{
-      id: 3,title:"Salary",amount:2000},{
-        id: 4,title:"Temple",amount:-250}
-      ]
-  )
-
-  useEffect(()=> {
-    axios.get(' http://localhost:5173/')
-    .then(res =>{
-      console.log(res.data)
-      setExpenses(res.data)
-    })
-    .catch(err => console.log(err))
-  })
-
+  const [expenses,setExpenses] = useState ([])
+    useEffect(() => {
+      axios.get('https://expense-front-end.onrender.com/get-entries')
+      .then(res => {
+        console.log(res.data)
+        setExpenses(res.data)
+      })
+      .catch(err => console.log(err))
+    },[])
 
   const addExpense = (title,amount) => {
     const nextId=expenses[expenses.length-1].id+1
